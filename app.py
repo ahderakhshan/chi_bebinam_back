cb8e885fc15740c4d51c9fa3e49ab033cd3e8e0a
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-08-22 23:31:25 +0430

    fix

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request
 import random
 import google.generativeai as genai
@@ -15,7 +17,8 @@ def movie_recommender():
     base_prompt = f"""As a movie recommendation system, your task is to suggest a suitable movie based on the user's answers to the questions. To do this, consider a pool of 100 movies and recommend the information of the movie with index {rand} that you select.
      These are questions and answer provide the movie title, genre, a short reason why it matches their profile, movie IMDB score, movie year of production, movie director and a summery of movie.\nyour answer must be in this form: Movie title: Movie title you recommend*Genre:Movie Genre you recommend*Description: a short description why you recommend this movie*Movie IMDB score: movie imdb score*Movie year of Production: movie year of production*Movie Director:movie director*Movie Summery:a summery of movie\nno other explanations at all."""
     data = request.get_json()
-    question_answers = data["question_answers"]
+    print(data)
+    question_answers = data["prompt"]["question_answers"]
     print(question_answers)
     for question_answer in question_answers:
         question = question_answer["question"]
@@ -34,7 +37,8 @@ def movie_recommender():
 
 
 if __name__ == '__main__':
-    env_file = load_dotenv("./.env")
-    genai.configure(api_key=env_file["API_KEY"])
+    env_file = load_dotenv(dotenv_path=".env")
+    api_key = os.getenv("API_KEY")
+    genai.configure(api_key=api_key)
     model = genai.GenerativeModel('gemini-1.5-flash')
     app.run(debug=True)
